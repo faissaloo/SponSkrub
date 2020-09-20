87abed471afd16772788d8a09e561dd65bb939de
pukkandan <pukkandan@gmail.com>
2020-09-21 00:21:24 +0530

    Change` -vcodec copy` to `-codec copy` in ffmpeg to prevent reencoding of the audio track

--- src/sponskrub/ffwrap.d
+++ src/sponskrub/ffwrap.d
@@ -65,7 +65,7 @@ bool add_ffmpeg_metadata(string input_filename, string output_filename, string m
 	}
 	write_metadata(metadata_filename, metadata);
 	
-	auto ffmpeg_process = spawnProcess(["ffmpeg", "-i", input_filename, "-i", metadata_filename, "-map_metadata", "0", "-map_chapters", "1", "-vcodec", "copy", output_filename]);
+	auto ffmpeg_process = spawnProcess(["ffmpeg", "-i", input_filename, "-i", metadata_filename, "-map_metadata", "0", "-map_chapters", "1", "-codec", "copy", output_filename]);
 	auto result = wait(ffmpeg_process) == 0;
 	
 	return result;
