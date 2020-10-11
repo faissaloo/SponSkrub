c06c12f73e5904454b6213be0d7ee37b0f05df8d
faissaloo <faissaloo@gmail.com>
2020-10-11 01:04:34 +0100

    Move timestamps_to_keep() to cut.d

--- src/sponskrub/methods/cut.d
+++ src/sponskrub/methods/cut.d
@@ -26,6 +26,31 @@ import std.typecons;
 import sponsorblock;
 import ffwrap;
 
+ClipTime[] timestamps_to_keep(ClipTime[] sponsor_times, string video_length) {
+	ClipTime[] clip_times;
+	sponsor_times.sort!((a, b) => a.start.to!float < b.start.to!float);
+	
+	//If the sponsorship is directly at the beginning don't add both content and the sponsor
+	if (sponsor_times[0].start != "0.000000") {
+		clip_times ~= ClipTime("0", sponsor_times[0].start, "content");
+	}
+
+	
+	for (auto i = 0; i < sponsor_times.length; i++) {
+		auto clip_start = "";
+		auto clip_end = "";
+		clip_start = sponsor_times[i].end;
+		if (i+1 < sponsor_times.length) {
+			clip_end = sponsor_times[i+1].start;
+		} else {
+			clip_end = video_length;
+		}
+		clip_times ~= ClipTime(clip_start, clip_end, "content");
+	}
+
+	return clip_times;
+}
+
 string cut_and_cat_clips_filter(ClipTime[] timestamps, FileCategory category) {
   timestamps.sort!((a, b) => a.start.to!float < b.start.to!float);
 
